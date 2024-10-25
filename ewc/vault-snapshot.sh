5f641998c9ac1418ff816f6a30fb145bf107940b
aaltonja <jani.aaltonen@cgi.com>
2024-10-25 12:12:34 +0300

    update script (only vault command not tested yet)

--- ewc/vault-snapshot.sh
+++ ewc/vault-snapshot.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
 
 # Variables
-VAULT_ADDR=${VAULT_ADDR:-"http://127.0.0.1:8200"}
-S3_BUCKET=${S3_BUCKET:-"your-s3-bucket-name"}
-SNAPSHOT_NAME="vault-snapshot-$(date +%Y-%m-%d_%H-%M-%S).snap"
+VAULT_ADDR=${VAULT_ADDR}
+VAULT_TOKEN=${VAULT_TOKEN}
+S3_BUCKET=${S3_BUCKET}
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+
+# Check if VAULT_ADDR is set
+if [ -z "$VAULT_ADDR" ]; then
+  echo "Error: VAULT_ADDR is not set."
+  exit 1
+fi
+
+# Check if VAULT_ADDR is set
+if [ -z "$VAULT_TOKEN" ]; then
+  echo "Error: VAULT_TOKEN is not set."
+  exit 1
+fi
+
+# Check if VAULT_ADDR is set
+if [ -z "$S3_BUCKET" ]; then
+  echo "Error: S3_BUCKET is not set."
+  exit 1
+fi
+
+# Check if AWS_ACCESS_KEY_ID is set
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "Error: AWS_ACCESS_KEY_ID is not set."
+  exit 1
+fi
+
+# Check if AWS_SECRET_ACCESS_KEY is set
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "Error: AWS_SECRET_ACCESS_KEY is not set."
+  exit 1
+fi
+
+# Generate ISO 8601 compliant timestamp
+# (needed to use sed as couldn't make it work with '%:z' in date command)
+TIMESTAMP_ISO_8601=$(date +%Y-%m-%dT%H:%M:%S$(date +%z | sed 's/\(..\)$/:\1/'))
+
+SNAPSHOT_NAME="snapshot-$TIMESTAMP_ISO_8601.snap"
 
 # Take the snapshot
 # https://developer.hashicorp.com/vault/tutorials/standard-procedures/sop-backup
